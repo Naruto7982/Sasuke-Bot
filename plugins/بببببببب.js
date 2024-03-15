b0e531d810e5988a0460bcb87d63a51dd741ae1b
Naruto7982 <147566293+Naruto7982@users.noreply.github.com>
2024-03-15 10:36:38 +0200

    Delete plugins directory

--- plugins/بببببببب.js
+++
@@ -1,8 +0,0 @@
-/* Generar error */
-const handler = async (m, {conn, command}) => {
-  console.log(XDDDDDDDDD);
-  new Error('ها هو الخطاء يا سيدي ايتاتشي');
-};
-handler.command = /^(اين-الخطاء)$/i;
-handler.owner = true;
-export default handler; 
